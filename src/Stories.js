07532d6453bd7f443366f0e868995bc10f2199f8
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-02-21 22:01:02 +0300

    creat stories content

--- src/Stories.js
+++ src/Stories.js
@@ -1,9 +1,53 @@
-import React from 'react'
-
-import { useGlobalContext } from './context'
+import React from "react";
+import moment from "moment";
+import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  return <h2>stories component</h2>
-}
+    const { isLoading, hits } = useGlobalContext();
+    if (isLoading) {
+        return <div className="loading"></div>;
+    }
+    return (
+        <section className="stories">
+            {hits.map((story) => {
+                const {
+                    objectID: id,
+                    title,
+                    url,
+                    num_comments,
+                    author,
+                    points,
+                    created_at: date,
+                } = story;
 
-export default Stories
+                return (
+                    <article key={id} className="story">
+                        <h4 className="title">{title}</h4>
+                        <p className="info">
+                            {points} points by <span> {author} | </span>
+                            {num_comments} comments
+                        </p>
+                        <p className="info">
+                            Created at{" "}
+                            {moment(date).format("MMMM Do, dddd, YYYY")}
+                        </p>
+                        <div>
+                            <a
+                                href={url}
+                                className="read-link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {" "}
+                                read more
+                            </a>
+                            <button className="remove-btn">remove</button>
+                        </div>
+                    </article>
+                );
+            })}
+        </section>
+    );
+};
+
+export default Stories;
