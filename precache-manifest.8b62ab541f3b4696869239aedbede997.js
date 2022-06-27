2687d71c352eabeb7924aff51f7b115380b97fd9
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-06-27 23:14:54 +0300

    Updates

---
+++ precache-manifest.8b62ab541f3b4696869239aedbede997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ded2a909c5c9a5430deb32acb7db70",
+    "url": "/React-HackerNews/index.html"
+  },
+  {
+    "revision": "4ea469240951876799c5",
+    "url": "/React-HackerNews/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "b5e30b4a767e09baff22",
+    "url": "/React-HackerNews/static/js/2.288aa3f7.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/React-HackerNews/static/js/2.288aa3f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea469240951876799c5",
+    "url": "/React-HackerNews/static/js/main.371fcb20.chunk.js"
+  },
+  {
+    "revision": "6c04b9a3528b32940268",
+    "url": "/React-HackerNews/static/js/runtime-main.206f225c.js"
+  }
+]);
